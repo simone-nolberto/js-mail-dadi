0c66cdf6317462113f1c336cd609d923ef6ff68b
simone-nolberto <simone.nolberto@outlook.com>
2024-02-22 15:24:30 +0100

    add
    base variable + user input + for structure

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let userEmail = prompt("Inserisci qui la tua email");
+console.log(userEmail);
+
+const party = ["simto1998@gmail.com", "simone.nolberto@leonori.it", "joyabela@outlook.com", "joy.abela@gmail.com"];
+console.log(party);
+
+for (let index = 0; index < party.length; index++) {
+    const element = party[index];
+    console.log(element);
+
+    // if (element === userEmail) {
+    //     console.log("Benvenuto, si accomodi!");
+    // } else {
+    //     console.log("Spiacente, lei non può entrare");
+    // }
+
+}
