25b89c7bd553346b5caf304833140d2bfc2ff4ef
simone-nolberto <simone.nolberto@outlook.com>
2024-02-22 16:23:17 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let userEmail = document.getElementById("usermail");
-console.log(userEmail);
+console.log(userEmail.value);
 
 const party = ["simto1998@gmail.com", "simone.nolberto@leonori.it", "joyabela@outlook.com", "joy.abela@gmail.com"];
 console.log(party);
@@ -16,7 +16,6 @@ trigger.addEventListener('click', function () {
             console.log("Benvenuto, si accomodi!");
 
         } else (email != element)
-
         console.log("Spiacente, lei non può entrare");
 
     }
@@ -24,22 +23,22 @@ trigger.addEventListener('click', function () {
 });
 
 
-
-
-
-
 // esercizio Dadi:
 
+const diceTrigger = document.querySelector(".dice")
+diceTrigger.addEventListener('click', function () {
 
-const userDice = Math.floor(Math.random() * 6) + 1;
-console.log(userDice);
+    const userDice = Math.floor(Math.random() * 6) + 1;
+    console.log(userDice);
 
-const pcDice = Math.floor(Math.random() * 6) + 1;
-console.log(pcDice);
+    const pcDice = Math.floor(Math.random() * 6) + 1;
+    console.log(pcDice);
 
 
-if (userDice > pcDice) {
-    console.log("Complimenti, ha vinto!");
-} else {
-    console.log("spiacenti, ritenti, sarà più fortunato");
-}
+    if (userDice > pcDice) {
+        console.log("Complimenti, ha vinto!");
+    } else {
+        console.log("spiacenti, ritenti, sarà più fortunato");
+    }
+
+})
